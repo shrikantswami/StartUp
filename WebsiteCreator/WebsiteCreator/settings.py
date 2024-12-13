892716b844eaf499e645001227513baafc7c030c
Shubhada1995 <shubhadahundekar95@gmail.com>
2024-12-13 20:02:35 +0530

    added site title logo

--- WebsiteCreator/WebsiteCreator/settings.py
+++ WebsiteCreator/WebsiteCreator/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-i99&24+h(2k1m9slf4%#4bmg+n($20yofv=5^s6kakgaby=$w2")
+SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / "uploads/"
 MEDIA_URL = "media/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
